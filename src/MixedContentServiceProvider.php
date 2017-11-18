10cb4b3a6515b4d77b8fa6404553a381396c860f
Alex Scott <codetheorist@gmail.com>
2017-11-18 23:58:47 +0000

    Service provider working

--- src/MixedContentServiceProvider.php
+++ src/MixedContentServiceProvider.php
@@ -6,17 +6,34 @@ use Illuminate\Support\ServiceProvider;
 
 class MixedContentServiceProvider extends ServiceProvider
 {
-  /**
-   * Bootstrap any application services.
-   *
-   * @return void
-   */
-  public function boot()
-  {
-    if (function_exists('config_path')) {
-      $this->publishes([
-        realpath(__DIR__.'/../config/config.php') => config_path('mixedcontent.php'),
-      ]);
+    /**
+     * This provider is deferred and should be lazy loaded.
+     *
+     * @var boolean
+     */
+    protected $defer = true;
+
+    /**
+    * Bootstrap any application services.
+    *
+    * @return void
+    */
+    public function boot()
+    {
+        if (function_exists('config_path')) {
+          $this->publishes([
+            __DIR__.'/../config/config.php' => config_path('mixedcontent.php'),
+          ], 'config');
+        }
+        $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'mixedcontent');
     }
-  }
+
+
+    /**
+     * Register IoC bindings.
+     */
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'mixedcontent');
+    }
 }
